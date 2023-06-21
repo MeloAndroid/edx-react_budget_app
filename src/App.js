b4568a78677f80a6b381f246a6e49ec14b411863
MeloAndroid <meloandroid0@gmail.com>
2023-06-21 15:50:43 -0400

    Ejercicio2: Añadidos ExpenseList y ExpenseItem

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Budget from './components/Budget';
 // Add code to import the other components here under
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
+                <div className='row mt-3'>
                         {/* Add Budget component here under */}   
                     <div className="col-sm"><Budget /></div>     
 
@@ -25,12 +26,16 @@ const App = () => {
                         {/* Add ExpenseTotal component here under */}
                     <div className="col-sm"><ExpenseTotal /></div>
                        
-                        {/* Add ExpenseList component here under */}         
-
                         {/* Add ExpenseItem component here under */}        
 
                         {/* Add AllocationForm component here under */}        
 
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                 </div>
             </div>
         </AppProvider>
